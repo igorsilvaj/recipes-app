a3a13c077644b57d9049caf8cf81234bb3767808
Daniel Oliveira <danoliveira865@gmail.com>
2023-01-26 16:16:10 -0300

    componente Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';

641a13746b1077dfbccb7a4e7878382125feadcb
PedrovRamos <pvramos.cunha@gmail.com>
2023-01-26 14:10:03 -0300

    Adicionando projeto com tela de login concluida faltando apenas testes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { saveUser } from '../redux/actions';
 
-export default function Login() {
+function Login({ dispatch }) {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [willRedirect, setWillRedirect] = useState(false);
+  const [userLogin, setUserLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  useEffect(() => {
+    const { email, password } = userLogin;
+    const SIX = 6;
+    const isEmailValid = email
+      .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (isEmailValid && password.length > SIX) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [userLogin]);
+
+  function handleChange({ target }) {
+    const { value, name } = target;
+    setUserLogin((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function handleClick() {
+    const { email } = userLogin;
+    localStorage.setItem('user', email);
+    dispatch(saveUser(email));
+    setWillRedirect(true);
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      { willRedirect && <Redirect to="/meals" /> }
+      <input
+        onChange={ handleChange }
+        name="email"
+        type="text"
+        data-testid="email-input"
+        value={ userLogin.email }
+      />
+      <input
+        onChange={ handleChange }
+        name="password"
+        type="password"
+        data-testid="password-input"
+        value={ userLogin.password }
+      />
+      <button
+        disabled={ isButtonDisabled }
+        onClick={ handleClick }
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </div>
   );
 }
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);

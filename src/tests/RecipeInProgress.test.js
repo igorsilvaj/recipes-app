d19bb190f2e46a42a28735d1ade3c3fda7b91cd7
IgorSilvaJ <igorjsilvabiz@gmail.com>
2023-01-29 19:14:21 -0300

    refatora teste do componente Profile, alteração no renderWithRouterAndRedux pois estava gerando erros

---
+++ src/tests/RecipeInProgress.test.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+// import App from '../App';
+// import reduxState from './mocks/requestMeal';
+
+describe('Testando componente <Header />', () => {
+  it('', () => {
+
+  });
+});

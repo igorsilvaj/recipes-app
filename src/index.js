e83ac2b86609b3421a5dcad0e0e3184642b0eec1
igorsilvaj <igorjsilvabiz@gmail.com>
2023-02-12 12:12:14 -0300

    feat: teste do hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
